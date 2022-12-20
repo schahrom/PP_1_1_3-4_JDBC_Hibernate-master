de0f3b66485d75dccde2e31c09bbecb727439bf4
Schahrom <schahrom1232939@gmail.com>
2022-12-20 07:46:45 +0300

    Minor fix for the mine method.

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -10,23 +10,17 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        // Создание таблицы User(ов)
-        // Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
-        // Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
-        // Очистка таблицы User(ов)
-        // Удаление таблицы
         UserDao userDao = new UserDaoJDBCImpl();
-//        userDao.createUsersTable();
-//        userDao.saveUser("Dima", "Tihomirov", (byte) 20);
-//        userDao.saveUser("Gadzi", "Gashimov", (byte) 23);
-//        userDao.saveUser("Kirill", "Gorshkov", (byte) 45);
-//        userDao.saveUser("Andrey", "Sergeevich", (byte) 45);
-//        for (User elem : userDao.getAllUsers()) {
-//            System.out.println(elem.toString());
-//            System.out.println("___________________________________________");
-//        }
-//        userDao.cleanUsersTable();
-//
+        userDao.createUsersTable();
+        userDao.saveUser("Dima", "Tihomirov", (byte) 20);
+        userDao.saveUser("Gadzi", "Gashimov", (byte) 23);
+        userDao.saveUser("Kirill", "Gorshkov", (byte) 45);
+        userDao.saveUser("Andrey", "Sergeevich", (byte) 45);
+        for (User elem : userDao.getAllUsers()) {
+            System.out.println(elem.toString());
+            System.out.println("___________________________________________");
+        }
+        userDao.cleanUsersTable();
         userDao.dropUsersTable();
     }
 }
